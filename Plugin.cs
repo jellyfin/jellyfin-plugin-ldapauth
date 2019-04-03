5fc1296eea110d2c59687556c9461ca4d6a03917
Phallacy <Dragoonmac@gmail.com>
2019-04-02 22:43:22 -0700

    Add configuration page

--- Plugin.cs
+++ Plugin.cs
@@ -4,10 +4,11 @@ using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Serialization;
 using MediaBrowser.Model.Plugins;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace Jellyfin.Plugin.LDAP_Auth
 {
-    public class Plugin : BasePlugin<PluginConfiguration>
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
         public static Plugin Instance { get; private set; }
         public static ILogger Logger{get; private set;}
@@ -18,5 +19,17 @@ namespace Jellyfin.Plugin.LDAP_Auth
 
         public override string Name => "LDAP-Auth";
         public override Guid Id => Guid.Parse("958aad66-3784-4d2a-b89a-a7b6fab6e25c");
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = this.Name,
+                    EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
+                }
+            };
+        }
     }
 }
